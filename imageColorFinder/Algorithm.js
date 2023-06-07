575771177b37c69e617e176c4df9d731c96b63db
surajbca <krsurajkumar7481@gmail.com>
2023-06-07 19:49:02 +0530

    setup project

---
+++ imageColorFinder/Algorithm.js
@@ -0,0 +1,41 @@
+function SquareRootCheckBox() {
+  let SimpleCheckBox = document.getElementById("Simple");
+  SimpleCheckBox.style.display = "none";
+
+  let LightColor = document.getElementById("LightColor");
+  LightColor.style.background = "#b5bcc6";
+
+  let SquareRoot = document.getElementById("SquareRoot");
+  SquareRoot.style.display = "";
+
+  let Dominant = document.getElementById("Dominant");
+  Dominant.style.display = "none";
+}
+
+function DominantCheckBox() {
+  let Dominant = document.getElementById("Dominant");
+  Dominant.style.display = "";
+
+  let LightColor = document.getElementById("LightColor");
+  LightColor.style.background = "#e8f5fe";
+
+  let SimpleCheckBox = document.getElementById("Simple");
+  SimpleCheckBox.style.display = "none";
+
+  let SquareRoot = document.getElementById("SquareRoot");
+  SquareRoot.style.display = "none";
+}
+
+function SimpleCheckBox() {
+  let Dominant = document.getElementById("Dominant");
+  Dominant.style.display = "none";
+
+  let SimpleCheckBox = document.getElementById("Simple");
+  SimpleCheckBox.style.display = "";
+
+  let LightColor = document.getElementById("LightColor");
+  LightColor.style.background = "#b5bcc6";
+
+  let SquareRoot = document.getElementById("SquareRoot");
+  SquareRoot.style.display = "none";
+}
